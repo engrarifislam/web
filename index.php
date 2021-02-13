b5135b380d23d139f4af0bcd974e8af4e7691bf4
Administrator <Administrator@ARIF-DM-IT>
2021-02-13 17:20:40 +0600

    initial commit

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <html>
     <head>
-        <title>title</title>
+        <title>PHP Project</title>
         <link rel="stylesheet" href="stylesheet.css" type="text/css"/>
     </head>
     <body>
+        <section id="mainheader">
+            <div class="logo">
+                <img href="images/logo.jpg" alt="logo"/>
+            </div>
+            <div class="menu"> 
+                <ul>
+                    <li <a href="#">Home</a></li>
+                    <li <a href="#">About us</a></li>
+                    <li <a href="#">Page</a></li>
+                    <li <a href="#">Testimonial</a></li>
+                    <li <a href="#">Contact us</a></li>
+
+                </ul>
+            </div>
+        </section>
 
     </body>
 </html>
